2476b148d193097419489256ab2071f55ac5c5a5
Brandon Fletcher <brandonjfletcher@gmail.com>
2023-01-19 18:03:44 -0600

    Initial commit.

---
+++ main.py
@@ -0,0 +1,14 @@
+# author: Brandon Fletcher
+# e-mail: brandonjfletcher@gmail.com
+# GitHub: https://github.com/bjfletc
+# crypt_keeper 🗝️:  an application to be used on macOS/Linux for the purpose of
+#                   encrypting/decrypting files using AES-256-CBC encryption that is built into
+#                   the operating system of these platforms.
+
+
+def main():
+    print('Hello, World! 🌎')
+
+
+if __name__ == '__main__':
+    main()
