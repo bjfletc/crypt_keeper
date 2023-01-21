44918e5b89ce4fe75b0f3ef86d9c84c8d861ef5d
Brandon Fletcher <brandonjfletcher@gmail.com>
2023-01-21 09:56:36 -0600

    create seperate module for the buttons, and import this module to the root_window.

---
+++ gui/buttons.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+
+def add_buttons(window):
+    button = Button(window, text='button')
+    return button
--- gui/root_window.py
+++ gui/root_window.py
@@ -1,11 +1,13 @@
 # going to be the launching point for the GUI part of the application
 
 from tkinter import *
+from gui.buttons import add_buttons
 
 
 def build_root():
     root = Tk()
     root.title('Crypt Keeper 🗝️')
     root.geometry('400x400')
+    add_buttons(root).pack()
 
     root.mainloop()
