91216c9b6f12f301cfac87a19de4b3fad20b506a
Brandon Fletcher <brandonjfletcher@gmail.com>
2023-01-20 20:11:02 -0600

    refactor code to reduce redundancy.

--- app_operations.py
+++ app_operations.py
@@ -1,26 +1,25 @@
 # contains the functions used to encrypt/decrypt files.
 
-# base command template, {operation} & {file_name} will be replaced by functions
-base_command = 'openssl enc -aes-256-cbc {operation} -in "{file_name}" -out "{file_name}"'
+def build_command(operation, file_name):
+    # base command template, {operation} & {file_name} will be replaced by function
+    base_command = 'openssl enc -aes-256-cbc {operation} -in "{file_name}" -out "{file_name}"'
+
+    # command = base_command
+
+    # set the operation to encrypt
+    base_command = base_command.replace('{operation}', operation)
+    base_command = base_command.replace('{file_name}', file_name, 1)
+    # check whether the file is an encrypted file
+    if file_name[-4:] == '.enc':
+        base_command = base_command.replace('{file_name}', file_name[:-4], -1)
+    else:
+        base_command = base_command.replace('{file_name}', file_name + '.enc', -1)
+    return base_command
 
 
 def encrypt_file(file_name):
-    global base_command
-    command = base_command
-
-    # set the operation to encrypt
-    command = command.replace('{operation}', '-e')
-    command = command.replace('{file_name}', file_name, 1)
-    command = command.replace('{file_name}', file_name + '.enc', -1)
-    return command
+    return build_command('-e', file_name)
 
 
 def decrypt_file(file_name):
-    global base_command
-    command = base_command
-
-    # set the operation to decrypt
-    command = command.replace('{operation}', '-d')
-    command = command.replace('{file_name}', file_name, 1)
-    command = command.replace('{file_name}', file_name[:-4], -1)
-    return command
+    return build_command('-d', file_name)
