403c0e400bf13c3a149a8085b02c66f8710cfe81
Brandon Fletcher <brandonjfletcher@gmail.com>
2023-01-30 13:08:26 -0600

    shortened import name to app

--- gui/root_window.py
+++ gui/root_window.py
@@ -3,20 +3,20 @@
 from gui.center_window_on_display import center_window
 from gui.component import *
 from btn_cmd_functions import choose_file, enter_key_command, run
-import gui.app_appearance
+import gui.app_appearance as app
 
 
 def build_root():
     root = Tk()
     root.title('Crypt Keeper 🔐')
-    root.config(bg=gui.app_appearance.COLOR)
+    root.config(bg=app.COLOR)
 
     # center the root window, and set width 900, height 500
-    center_window(root, 900, 500)
+    center_window(root, app.WIDTH, app.HEIGHT)
 
-    left_component = Component(root, gui.app_appearance.COLOR, 'Choose File 📂', choose_file)
-    middle_component = Component(root, gui.app_appearance.COLOR, 'Cryptographic Key 🔑')
-    right_component = Component(root, gui.app_appearance.COLOR, 'Encrypt File 🔒', run)
+    left_component = Component(root, app.COLOR, 'Choose File 📂', choose_file)
+    middle_component = Component(root, app.COLOR, 'Cryptographic Key 🔑')
+    right_component = Component(root, app.COLOR, 'Encrypt File 🔒', run)
     middle_component.button.config(command=lambda: enter_key_command(right_component))
 
     root.mainloop()
